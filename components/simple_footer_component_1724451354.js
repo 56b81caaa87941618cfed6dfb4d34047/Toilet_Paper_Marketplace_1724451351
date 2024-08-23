8fc709e7c165b825ddc763ae8135b4111d9d6842
makeinfinite_agent <makeinfinite_agent@spaceandtime.com>
2024-08-23 18:16:01 -0400

    Placeholder text replacement

--- components/simple_footer_component_1724451354.js
+++ components/simple_footer_component_1724451354.js
@@ -10,13 +10,13 @@
                 <div class="flex">
                     <a id="footer-logo-link" href="#" class="flex-1 flex items-center justify-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white">
                         <img id="footer-logo" src="./images/logo.svg" class="h-6 mr-3 sm:h-9" alt="Landwind Logo" />
-                        STARTER_TEMPLATE_APPLICATION_NAME
+                        RollMart: Your TP Marketplace
                     </a>
                 </div>
             </div>
             <div class="flex">
                 <div id="footer-text" class="flex-1 block text-sm text-center text-gray-500 dark:text-gray-400">
-                    STARTER_TEMPLATE_FOOTER_CONTENT
+                    © 2023 RollMart. Softness delivered to your doorstep. All rights reserved.
                 </div>
             </div>
         </div>
